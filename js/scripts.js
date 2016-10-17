0459a09b62f21c650db17f117e733155fd24370a
Jonathan Thom and Tracie Weitzman <jonathan@>
2016-10-17 10:04:22 -0700

    Added other functions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ var add = function(number1, number2){
 	return number1 + number2;
 };
 
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
+
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+var result = divide(number1, number2);
+alert(result);
